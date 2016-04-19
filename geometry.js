251d86fe1908e410d82b129779a8ceaa40dd3238
Petru Dimitriu <dimitriupetru@gmail.com>
2016-04-20 00:20:45 +0300

    Segments & convex hull

--- geometry.js
+++ geometry.js
@@ -22,8 +22,8 @@
 
     Segment = function(point1, point2, color)
     {
-      this.point1 = point1;
-      this.point2 = point2;
+      this.point1 = new Point(point1.x,point1.y);
+      this.point2 = new Point(point2.x,point2.y);
       if (color === undefined)
         this.color = "#000000";
       else
